e0aa494a8f9f53d4e63736c2ca72fad1242c4f14
Gediminas Zamulskis <g.zamulskis@gmail.com>
2023-06-01 11:45:29 +0300

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.SingleAsync(u => u.Id == id);
+            return Ok(user);
+        }
+    }
+}
